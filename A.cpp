ea08a4648048c5b7d6b62797e427f7c1aebecbff
Nomad192 <verenya08@gmail.com>
2023-01-04 03:14:49 +0300

    A

---
+++ A.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <set>
+
+typedef unsigned long long uint;
+
+struct queue_t {
+    uint n = 0;
+    unsigned long long fine = 0;
+    std::set<size_t> available_seats;
+
+    explicit queue_t(size_t n, size_t max_d = SIZE_MAX) : n(n) {
+        for (size_t i = 1; i <= std::min(n, max_d); i++)
+            available_seats.insert(i);
+    }
+
+    void add(uint d, uint w) {
+        if (d == 0)
+            fine += w;
+        else if (d < n) {
+            auto iter = available_seats.upper_bound(d);
+
+            if (iter != available_seats.begin()) {
+                iter--;
+                available_seats.erase(iter);
+            } else
+                fine += w;
+        }
+    }
+};
+
+using task_t = std::pair<uint, uint>;
+struct task_comparator_t
+{
+    bool operator() (task_t const &one, task_t const &two)
+    {
+        return one.second > two.second;
+    }
+};
+
+unsigned long long a_work(size_t n, uint *array) {
+    std::multiset<task_t, task_comparator_t> sort_array;
+    uint max_d = array[0];
+    for (size_t i = 0; i < n; i++)
+    {
+        if (array[(2 * i) + 1] != 0)
+        {
+            sort_array.insert({array[2 * i], array[(2 * i) + 1]});
+            if(array[2 * i] > max_d)
+                max_d = array[2 * i];
+        }
+    }
+
+    queue_t queue(n);
+
+    for (const auto &item: sort_array)
+        queue.add(item.first, item.second);
+
+
+    return queue.fine;
+}
+
+#ifndef TEST_ON
+
+#include <fstream>
+
+int main() {
+    std::ifstream in_file;
+    std::ofstream out_file;
+
+    in_file.open("schedule.in");
+    out_file.open("schedule.out");
+
+    ///==================================================///
+    std::istream &in = in_file;             /// = std::cin
+    std::ostream &out = out_file;         /// = std::cout
+    ///==================================================///
+
+    const auto n = [&in](size_t t) { return in >> t, std::move(t); }({});
+
+    uint *array = new uint[2 * n];
+
+    for (size_t i = 0; i < 2 * n; i++)
+        in >> array[i];
+
+    out << a_work(n, array) << "\n";
+
+    delete[] array;
+    return 0;
+}
+
+#endif
---
+++ A_Test.cpp
@@ -0,0 +1,241 @@
+#include <iostream>
+#include "gtest/gtest.h"
+#include "A.cpp"
+
+#ifdef TEST_ON
+
+TEST(correctness, 0)
+{
+    uint array[] = {
+            2,
+            1, 1,
+            1, 2,
+    };
+
+    ASSERT_EQ(1, a_work(array[0], array + 1));
+}
+
+TEST(correctness, 1)
+{
+    uint array[] = {
+            2,
+            1, 2,
+            1, 2,
+    };
+
+    ASSERT_EQ(2, a_work(array[0], array + 1));
+}
+
+TEST(correctness, 2)
+{
+    uint array[] = {
+            6,
+            1, 0,
+            1, 3,
+            2, 4,
+            1, 5,
+            1, 1,
+            1, 0,
+    };
+
+    ASSERT_EQ(4, a_work(array[0], array + 1));
+}
+
+TEST(correctness, 3)
+{
+    uint array[] = {
+            5,
+            1, 5,
+            1, 4,
+            1, 3,
+            1, 2,
+            2, 100,
+    };
+
+    ASSERT_EQ(9, a_work(array[0], array + 1));
+}
+
+TEST(correctness, 4)
+{
+    uint array[] = {
+            6,
+            1, 5,
+            1, 4,
+            1, 3,
+            1, 2,
+            2, 100,
+            2, 120,
+    };
+
+    ASSERT_EQ(14, a_work(array[0], array + 1));
+}
+
+TEST(correctness, 5)
+{
+    uint array[] = {
+            6,
+            1, 1,
+            2, 2,
+            3, 3,
+            4, 4,
+            5, 5,
+            6, 6,
+    };
+
+    ASSERT_EQ(0, a_work(array[0], array + 1));
+}
+
+TEST(correctness, 6)
+{
+    uint array[] = {
+            6,
+            1, 1,
+            2, 2,
+            3, 3,
+            4, 4,
+            5, 5,
+            2, 6,
+    };
+
+    ASSERT_EQ(1, a_work(array[0], array + 1));
+}
+
+TEST(correctness, 7)
+{
+    uint array[] = {
+            4,
+            1, 15,
+            2, 100,
+            2, 25,
+            1, 30,
+    };
+
+    ASSERT_EQ(40, a_work(array[0], array + 1));
+}
+
+TEST(correctness, 8)
+{
+    uint array[] = {
+            3,
+            1, 15,
+            100, 100,
+            100, 100,
+    };
+
+    ASSERT_EQ(0, a_work(array[0], array + 1));
+}
+
+TEST(correctness, 9)
+{
+    uint array[] = {
+            3,
+            1, 15,
+            0, 100,
+            100, 100,
+    };
+
+    ASSERT_EQ(100, a_work(array[0], array + 1));
+}
+
+TEST(correctness, 10)
+{
+    uint array[] = {
+            3,
+            1, 15,
+            2, 100,
+            100, 100,
+    };
+
+    ASSERT_EQ(0, a_work(array[0], array + 1));
+}
+
+TEST(correctness, 11)
+{
+    uint array[] = {
+            4,
+            1, 15,
+            3, 100,
+            3, 200,
+            100, 100,
+    };
+
+    ASSERT_EQ(0, a_work(array[0], array + 1));
+}
+
+TEST(correctness, 12)
+{
+    uint array[] = {
+            6,
+            1, 1,
+            3, 3,
+            3, 3,
+            3, 3,
+            3, 3,
+            4, 100,
+    };
+
+    ASSERT_EQ(4, a_work(array[0], array + 1));
+}
+
+TEST(correctness, 13)
+{
+    uint array[] = {
+            6,
+            1, 1,
+            5, 3,
+            5, 3,
+            5, 3,
+            5, 3,
+            6, 100,
+    };
+
+    ASSERT_EQ(0, a_work(array[0], array + 1));
+}
+
+
+TEST(correctness, 14)
+{
+    uint array[] = {
+            4,
+            3, 4,
+            3, 2,
+            1, 1,
+            3, 2,
+    };
+
+    ASSERT_EQ(1, a_work(array[0], array + 1));
+}
+
+TEST(correctness, 15)
+{
+    uint array[] = {
+            4,
+            1, 10000000000,
+            1, 10000000000,
+            1, 10000000000,
+            1, 10000000000,
+    };
+
+    ASSERT_EQ(30000000000, a_work(array[0], array + 1));
+}
+
+TEST(correctness, 16)
+{
+    uint array[] = {
+            10,
+            5, 2,
+            0, 0,
+            3, 9,
+            1, 8,
+            1, 9,
+            5, 3,
+            2, 5,
+            2, 5,
+            8, 6,
+            7, 7,
+    };
+
+    ASSERT_EQ(13, a_work(array[0], array + 1));
+}
+
+#endif /// TEST_ON == 1
